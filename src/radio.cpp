3a5b2580630c4083b2c990315d7b262cf949bc67
Paul Miailhe <38831329+axpaul@users.noreply.github.com>
2026-05-23 20:13:04 +0200

    feat: translate OLED invalid frame error message to English

--- src/radio.cpp
+++ src/radio.cpp
@@ -172,7 +172,7 @@ size_t RadioReceive(U8G2_SSD1306_128X64_NONAME_F_HW_I2C* u8g2, SX1276 *radio, ui
           activeTrackersCount++;
         }
       } else {
-        strcpy(dispSsidApid, "Trame invalide (<3B)");
+        strcpy(dispSsidApid, "Invalid frame (<3B)");
       }
       
       dispRssi = radio->getRSSI();
